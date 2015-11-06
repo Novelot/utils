1f1b7fd9c7acf28e3ecc84e4264c7b188af0a1e4
liuyl <liuyl@kaolafm.com>
2015-11-06 18:26:25 +0800

    init

---
+++ src/main/java/com/novelot/utils/Logger.java
@@ -0,0 +1,28 @@
+package com.novelot.utils;
+
+import android.util.Log;
+
+/**
+ * Created by V on 2015/8/27.
+ */
+public class Logger {
+    public static void v(String tag, String msg) {
+        Log.v(tag, msg);
+    }
+
+    public static void i(String tag, String msg) {
+        Log.i(tag, msg);
+    }
+
+    public static void d(String tag, String msg) {
+        Log.d(tag, msg);
+    }
+
+    public static void w(String tag, String msg) {
+        Log.w(tag, msg);
+    }
+
+    public static void e(String tag, String msg) {
+        Log.e(tag, msg);
+    }
+}
